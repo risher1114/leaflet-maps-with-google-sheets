5ba02eea4d8aae3aaff08c193ec04a3ad30d992b
risher1114 <ruixue6618@gmail.com>
2019-01-28 11:34:53 -0500

    Update google-doc-url.js

--- google-doc-url.js
+++ google-doc-url.js
@@ -1,1 +1,1 @@
-var googleDocURL = 'https://docs.google.com/spreadsheets/d/1ZxvU8eGyuN9M8GxTU9acKVJv70iC3px_m3EVFsOHN9g/edit#gid=0';
+var googleDocURL = 'https://docs.google.com/spreadsheets/d/1VAmPHKLTju0KehEDx0UlbILuqoE9Nla5kBkvjgaOBhg/edit#gid=0';
